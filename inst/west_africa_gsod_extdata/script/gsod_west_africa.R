c082ecb798ab4b7ce0ad22e03913c1793b7bd1bf
ecor <emanuele.cordano@gmail.com>
2021-08-13 17:23:46 +0200

    local updates

--- inst/west_africa_gsod_extdata/script/gsod_west_africa.R
+++ inst/west_africa_gsod_extdata/script/gsod_west_africa.R
@@ -13,3 +13,4 @@ west_africa <- system.file("west_africa_gsod_extdata/west_africa.csv",package="S
 system.time(out <- get_GSOD(year=1980:1982,country=west_africa$ISO_3166_1[-1][1]))
 ###west_africa <- read_csv("inst/west_africa_gsod_extdata/west_africa.csv")
 ##View(west_africa)
+### AMMA : http://bd.amma-catch.org/main.jsf
