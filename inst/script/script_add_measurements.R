7b602b3a3e002dab65c2c327d74c61bdbbf7434e
rendena100 <emanuele.cordano@gmail.com>
2021-08-13 17:22:14 +0200

    gsod bf

--- inst/script/script_add_measurements.R
+++ inst/script/script_add_measurements.R
@@ -4,9 +4,9 @@
 library(SuSnowDB)
 library(RPostgreSQL) 
 
-source('~/activity/2021/local/SuSnowDB/R/add_locations_into_ssdb.R')
-source('~/activity/2021/local/SuSnowDB/R/add_measurement_types_into_ssdb.R')
-source('~/activity/2021/local/SuSnowDB/R/add_measurements_into_ssdb.R')
+##source('~/activity/2021/local/SuSnowDB/R/add_locations_into_ssdb.R')
+##source('~/activity/2021/local/SuSnowDB/R/add_measurement_types_into_ssdb.R')
+##source('~/activity/2021/local/SuSnowDB/R/add_measurements_into_ssdb.R')
 
 
 dbname <- "hydroclimatedb_testing00"
@@ -19,8 +19,8 @@ add_measurements_into_ssdb(conn,val$measurements)
 
 
 ### Add weather station from MeteoTrentino network, Trentino, Italy 
-##val_smet <- meteotrentino_smet_dataset(smet_files=c("T0175","T0179"))
-val_smet <- meteotrentino_smet_dataset()
+val_smet <- meteotrentino_smet_dataset(smet_files=c("T0175","T0179"))
+##val_smet <- meteotrentino_smet_dataset()
 add_locations_into_ssdb(conn,val_smet$locations,append=TRUE)
 add_measurement_types_into_ssdb(conn,val_smet$measurement_type,append=TRUE)
 add_measurements_into_ssdb(conn,val_smet$measurements,append=TRUE)
